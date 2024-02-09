c8997836e6ec6d3f290f4e34fc88f65781c15383
putrarmdhnd <ramadhan.dewantoro95@gmail.com>
2024-02-09 15:00:52 +0700

    i thougt mine was done

--- resources/views/kelola_data_masyarakat/bps.blade.php
+++ resources/views/kelola_data_masyarakat/bps.blade.php
@@ -1,99 +1,131 @@
 @extends('templates/dashboard')
 
-<div class="mx-auto w-full mt-24 px-10 lg:px-4">
-    <div class="bg-white py-2 mb-5 rounded-lg">
-        <div class="row d-flex align-items-center">
-            <div class="col-8">
-                <h1 class="text-lg lg:text-2xl headDash fw-bolder mb-2">Data Bantuan Pangan Stunting Penduduk Desa Palasari</h1>
+@section('content')
+
+<div class="layoutWelcome bg-white py-4 px-9 mb-5 rounded-lg">
+    <div class="row d-flex align-items-center">
+        <div class="col-12">
+            <h1 class="text-lg lg:text-2xl headDash fw-bolder mb-2">Data Bantuan Pangan Stunting Penduduk Desa Palasari</h1>
+            <p class="textDashboard text-base text-[13px] lg:text-lg font-normal text-secondary">Semua data BPS dari masyarakat Desa Palasari Kecamatan Cipanas</p>
+        </div>
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <form action="/data/importbps" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        <div class="modal-body">
+                            <div class="form-group">
+                                <input type="file" name="file" required>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-primary">Save changes</button>
+                        </div>
+                    </form>
+                </div>
             </div>
-            <div class="col-4 d-flex justify-content-center">
-                <div class="row d-block">
-                    <a href="/data/input-bps" class="text-black focus:outline-none font-medium rounded-lg text-sm px-4 py-2.5 text-center text-decoration-none" style="background-color: #b7efff;">Tambah Data</a>
-                    <a href="#" class="text-black focus:outline-none font-medium rounded-lg text-sm px-4 py-2.5 text-center text-decoration-none" style="background-color: #b7efff;" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        </div>
+    </div>
+</div>
+
+<div class="card">
+    <div class="card-header bg-white py-3">
+        <div class="col-12">
+            <div id="btn__up_mobileKependudukan" class="col-md-6 d-flex justify-content-end py-2">
+                <div class="layoutBtnPengaduan">
+                    <a href="/data/input-bps" class="btnPengaduan text-black focus:outline-none font-medium rounded-lg text-sm px-4 py-2.5 text-center text-decoration-none" style="background-color: #b7efff;"><i class="bx bxs-plus-circle"></i> Tambah Data</a>
+                    <a href="#" class="btnPengaduan text-black focus:outline-none font-medium rounded-lg text-sm px-4 py-2.5 text-center text-decoration-none ms-2" style="background-color: #b7efff;" data-bs-toggle="modal" data-bs-target="#exampleModal">
                         Import Data
                     </a>
-
                 </div>
-
-
-                <!-- Modal -->
-                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-                            <form action="/data/importbps" method="POST" enctype="multipart/form-data">
-                                @csrf
-                                <div class="modal-body">
-                                    <div class="form-group">
-                                        <input type="file" name="file" required>
-                                    </div>
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                    <button type="submit" class="btn btn-primary">Save changes</button>
-                                </div>
-                            </form>
-                        </div>
+            </div>
+            <div class="row align-items-center py-2">
+                <div class="col-md-6 py-2">
+                    <div class="input-group">
+                        <input type="text" id="searchInput" class="form-control" placeholder="Search">
+                    </div>
+                </div>
+                <div id="btn__down_largeKependudukan" class="col-md-6 d-flex justify-content-end py-2">
+                    <div class="layoutBtnPengaduan">
+                        <a href="/data/input-bps" class="btnPengaduan text-black focus:outline-none font-medium rounded-lg text-sm px-4 py-2.5 text-center text-decoration-none" style="background-color: #b7efff;"><i class="bx bxs-plus-circle"></i> Tambah Data</a>
+                        <a href="#" class="btnPengaduan text-black focus:outline-none font-medium rounded-lg text-sm px-4 py-2.5 text-center text-decoration-none ms-2" style="background-color: #b7efff;" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                            <i class="bx bx-import"></i>
+                            Import Data
+                        </a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    <div class="card-body">
+        <div class="w-full rounded-lg bg-white divide-y  overflow-x-auto divide-gray overflow-x-auto mb-5">
 
+            <table id="pendudukTable" class="table table-bordered border w-full rounded-lg bg-white divide-y divide-gray overflow-hidden">
+                <thead class="themeColor text-white text-center">
+                    <tr>
+                        <th rowspan="2" class="textTabelTop font-semibold text-sm uppercase px-4 py-3 align-middle">No</th>
+                        <th rowspan="2" class="textTabelTop font-semibold text-sm uppercase px-4 py-3 align-middle">NIK</th>
+                        <th rowspan="2" class="textTabelTop font-semibold text-sm uppercase px-4 py-3 align-middle">Nama Lengkap</th>
+                        {{-- <th rowspan="2" class="textTabelTop font-semibold text-sm uppercase px-4 py-3 align-middle">Tempat Tanggal Lahir</th>
+                    <th rowspan="2" class="textTabelTop font-semibold text-sm uppercase px-4 py-3 align-middle">Tempat Tanggal Kematian</th>
+                    <th rowspan="2" class="textTabelTop font-semibold text-sm uppercase px-4 py-3 align-middle">Nama Pelapor</th>
+                    <th rowspan="2" class="textTabelTop font-semibold text-sm uppercase px-4 py-3 align-middle">NIK Pelapor</th>
+                    <th rowspan="2" class="textTabelTop font-semibold text-sm uppercase px-4 py-3 align-middle">Nomor Yang Bisa Di Hubungi</th> --}}
+                        <th colspan="2" class="textTabelTop font-semibold text-sm uppercase px-4 py-3 align-middle">Aksi</th>
+                    </tr>
+                    <tr>
 
-    <div class="col-12 my-5 mx-0">
-        <table class="table table-bordered border ">
-            <thead class="themeColor text-white">
-                <tr>
-                    <th rowspan="2" class="text-center align-middle font-table">No</th>
-                    <th rowspan="2" class="text-center align-middle font-table">NIK</th>
-                    <th rowspan="2" class="text-center align-middle font-table">Nama Lengkap</th>
-                    {{-- <th rowspan="2" class="text-center align-middle font-table">Tempat Tanggal Lahir</th>
-                    <th rowspan="2" class="text-center align-middle font-table">Tempat Tanggal Kematian</th>
-                    <th rowspan="2" class="text-center align-middle font-table">Nama Pelapor</th>
-                    <th rowspan="2" class="text-center align-middle font-table">NIK Pelapor</th>
-                    <th rowspan="2" class="text-center align-middle font-table">Nomor Yang Bisa Di Hubungi</th> --}}
-                    <th colspan="2" class="text-center align-middle font-table">Aksi</th>
-                </tr>
-                <tr>
-                   
-                    <th class="text-center align-middle font-table">Detail</th>
-                    <th class="text-center align-middle font-table">Hapus</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($bps as $item)
-                <tr>
-                    <td class="text-center align-middle font-table">{{ $item->id }}</td>
-                    <td class="text-center align-middle font-table">{{ $item->NIK }}</td>
-                    <td class="text-center align-middle font-table">{{ $item->namaLengkap }}</td>
-                    {{-- <td class="text-center align-middle font-table">{{ $item->ttl }}</td>
-                    <td class="text-center align-middle font-table">{{ $item->ttm }}</td>
-                    <td class="text-center align-middle font-table">{{ $item->namaPelapor }}</td>
-                    <td class="text-center align-middle font-table">{{ $item->nikPelapor }}</td>
-                    <td class="text-center align-middle font-table">{{ $item->noDapatDihubungi }}</td> --}}
-                    <td class="text-center align-middle font-table">
-                        <button class="text-primary text-center detailKependudukan" data-user="">
-                            <i class='bx bx-edit-alt'></i>
-                        </button>
-                    </td>
-                    <td class="text-center align-middle font-table">
-                        <button class="text-danger text-center deletePenduduk" data-id="{{ $item->id }}">
-                            <i class="bx bxs-trash"></i>
-                        </button>
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
+                        <th class="textTabelTop font-semibold text-sm uppercase px-4 py-3 align-middle">Detail</th>
+                        <th class="textTabelTop font-semibold text-sm uppercase px-4 py-3 align-middle">Hapus</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($bps as $item)
+                    <tr>
+                        <td class="textTable px-2 py-4 text-secondary align-middle">{{ $item->id }}</td>
+                        <td class="textTable px-2 py-4 text-secondary align-middle">{{ $item->NIK }}</td>
+                        <td class="textTable px-2 py-4 text-secondary align-middle">{{ $item->namaLengkap }}</td>
+                        {{-- <td class="textTable px-2 py-4 text-secondary align-middle">{{ $item->ttl }}</td>
+                        <td class="textTable px-2 py-4 text-secondary align-middle">{{ $item->ttm }}</td>
+                        <td class="textTable px-2 py-4 text-secondary align-middle">{{ $item->namaPelapor }}</td>
+                        <td class="textTable px-2 py-4 text-secondary align-middle">{{ $item->nikPelapor }}</td>
+                        <td class="textTable px-2 py-4 text-secondary align-middle">{{ $item->noDapatDihubungi }}</td> --}}
+                        <td class="textTable px-2 py-4 text-secondary align-middle">
+                            <button class="text-primary text-center detailKependudukan" data-user="">
+                                <i class='bx bx-edit-alt'></i>
+                            </button>
+                        </td>
+                        <td class="textTable px-2 py-4 text-secondary align-middle">
+                            <button class="text-danger text-center deletePenduduk" data-id="{{ $item->id }}">
+                                <i class="bx bxs-trash"></i>
+                            </button>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            <div class="d-flex justify-content-center mt-4">
 
-
-
-
+            </div>
+            <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+            <script>
+                $(document).ready(function() {
+                    $('#searchInput').on('keyup', function() {
+                        var value = $(this).val().toLowerCase();
+                        $('#pendudukTable tbody tr').filter(function() {
+                            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+                        });
+                    });
+                });
+            </script>
+        </div>
     </div>
 </div>
 
-</div>
+@endsection
--- resources/views/landing_page/cms/apb_desa/index.blade.php
+++ resources/views/landing_page/cms/apb_desa/index.blade.php
@@ -1,101 +1,127 @@
 @extends('landing_page.cms.layouts.app')
 @section('content')
-    <div class="container-fluid">
+<div class="container-fluid">
 
-        @if (session('success'))
-            <div class="alert alert-success mt-3" role="alert">
-                {{ session('success') }}
-            </div>
-        @endif
+    @if (session('success'))
+    <div class="alert alert-success mt-3" role="alert">
+        {{ session('success') }}
+    </div>
+    @endif
 
-        @if (session('error'))
-            <div class="alert alert-danger mt-3" role="alert">
-                {{ session('error') }}
-            </div>
-        @endif
-        <!-- Page Heading -->
-        <div class="d-sm-flex align-items-center justify-content-between mb-4">
-            <h1 class="h3 mb-0 text-gray-800">Data APB Desa</h1>
-            <a href="{{ route('cms.apb.create', ['type' => 'pengeluaran']) }}" class="btn btn-sm btn-primary shadow-sm mt-3 mt-md-0 mt-lg-0"><i
-                    class="fas fa-plus-circle"></i>
-                Tambah Kegiatan</a>
-            <a href="{{ route('cms.apb.create', ['type' => 'apb']) }}" class="btn btn-sm btn-primary shadow-sm mt-3 mt-md-0 mt-lg-0"><i
-                    class="fas fa-plus-circle"></i>
-                Tambah APB Desa</a>
+    @if (session('error'))
+    <div class="alert alert-danger mt-3" role="alert">
+        {{ session('error') }}
+    </div>
+    @endif
+    <!-- Page Heading -->
+    <div class="bg-white py-4 px-9 mb-3 rounded-lg flex justify-between items-center">
+        <div class="">
+            <h1 class="text-lg lg:text-2xl headDash font-bold mb-2">Data APB Desa</h1>
+            <p class="text-base font-normal text-secondary">Data dari setiap pengeluaran dan dana masuk APBDes Desa Palasari</p>
         </div>
+    </div>
 
-        @if ($errors->any())
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
 
-        <div class="card shadow mb-4">
-            <div class="card-header py-3">
-                <h6 class="m-0 font-weight-bold text-primary">Data APB Desa</h6>
-            </div>
-            <div class="card-body">
-                <div class="table-responsive">
-                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                        <thead>
-                            <tr>
-                                <th>No</th>
-                                <th>Tahun</th>
-                                <th>Anggaran</th>
-                                <th>Judul Kegiatan</th>
-                                <th>Pengeluaran</th>
-                                <th>Aksi</th>
-                            </tr>
-                        </thead>
-
-                        <tbody>
-                            @foreach ($apb_desas as $item)
-                                <tr>
-                                    <td>{{ $loop->iteration }}</td>
-                                    <td>{{ $item->judul }} - Tahun {{ $item->tahun }}</td>
-                                    <td>{{ number_format($item->anggaran, 0, ',', '.') }}</td>
-                                    <td>{{ $item->judulPengeluaran }}</td>
-                                    <td>{{ number_format($item->pengeluaran, 0, ',', '.') }}</td>
-                                    <td>
-                                        <div class="d-flex justify-content-evenly">
-                                            <div class="pr-2">
-                                                <a href="{{ route('cms.apb.edit', $item->id) }}"
-                                                    class="btn btn-info btn-sm me-3">View</a>
-                                            </div>
-                                            <div class="pr-2">
-                                                <a href="{{ $item->tahun !== null ? route('cms.apb.edit', $item->id) : route('cms.apb.editPengeluaran', $item->id) }}"
-                                                    class="btn btn-primary btn-sm me-3">Edit</a>
-                                            </div>
-                                            
-                                            <form action="{{ route('cms.apb.destroy', $item->id) }}" method="post">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit" class="btn btn-danger btn-sm">Hapus</button>
-                                            </form>
-                                        </div>
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+    <div class="card ">
+        <div class="card-header bg-white py-3">
+            <div class="col-12">
+                <div id="btn__up_mobileKependudukan" class="col-md-6 d-flex justify-content-end py-2">
+                    <div class="layoutBtnPengaduan">
+                        <a href="{{ route('cms.apb.create', ['type' => 'pengeluaran']) }}" class="btnPengaduan text-black focus:outline-none font-medium rounded-lg text-sm px-4 py-2.5 text-center text-decoration-none" style="background-color: #b7efff;"><i class="fas fa-plus-circle"></i> Kegiatan</a>
+                        <a href="{{ route('cms.apb.create', ['type' => 'apb']) }}" class="btnPengaduan text-black focus:outline-none font-medium rounded-lg text-sm px-4 py-2.5 text-center text-decoration-none" style="background-color: #b7efff;"><i class="fas fa-plus-circle"></i> APBDes</a>
+                    </div>
+                </div>
+                <div class="row align-items-center py-2">
+                    <div class="col-md-6 py-2">
+                        <div class="input-group">
+                            <input type="text" id="searchInput" class="form-control" placeholder="Search">
+                        </div>
+                    </div>
+                    <div id="btn__down_largeKependudukan" class="col-md-6 d-flex justify-content-end py-2">
+                        <div class="layoutBtnPengaduan">
+                            <a href="{{ route('cms.apb.create', ['type' => 'pengeluaran']) }}" class="btnPengaduan text-black focus:outline-none font-medium rounded-lg text-sm px-4 py-2.5 text-center text-decoration-none" style="background-color: #b7efff;"><i class="fas fa-plus-circle"></i> Kegiatan</a>
+                            <a href="{{ route('cms.apb.create', ['type' => 'apb']) }}" class="btnPengaduan text-black focus:outline-none font-medium rounded-lg text-sm px-4 py-2.5 text-center text-decoration-none" style="background-color: #b7efff;"><i class="fas fa-plus-circle"></i> APBDes</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
+        <div class="card-body">
+            <div class="w-full rounded-lg bg-white divide-y divide-gray overflow-x-auto mb-5">
+                <table id="informasiTable" class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead class="themeColor text-white text-center">
+                        <tr>
+                            <th class="textTabelTop font-semibold text-sm uppercase px-4 py-2 align-middle">No</th>
+                            <th class="textTabelTop font-semibold text-sm uppercase px-4 py-2 align-middle">Tahun</th>
+                            <th class="textTabelTop font-semibold text-sm uppercase px-4 py-2 align-middle">Anggaran</th>
+                            <th class="textTabelTop font-semibold text-sm uppercase px-4 py-2 align-middle">Judul Kegiatan</th>
+                            <th class="textTabelTop font-semibold text-sm uppercase px-4 py-2 align-middle">Pengeluaran</th>
+                            <th class="textTabelTop font-semibold text-sm uppercase px-4 py-2 align-middle">Aksi</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        @foreach ($apb_desas as $item)
+                        <tr>
+                            <td class="textTable px-2 py-4 text-secondary align-middle align-middle">{{ $loop->iteration }}</td>
+                            <td class="textTable px-2 py-4 text-secondary align-middle align-middle">{{ $item->judul }} - Tahun {{ $item->tahun }}</td>
+                            <td class="textTable px-2 py-4 text-secondary align-middle align-middle">{{ number_format($item->anggaran, 0, ',', '.') }}</td>
+                            <td class="textTable px-2 py-4 text-secondary align-middle align-middle">{{ $item->judulPengeluaran }}</td>
+                            <td class="textTable px-2 py-4 text-secondary align-middle align-middle">{{ number_format($item->pengeluaran, 0, ',', '.') }}</td>
+                            <td class="textTable px-2 py-4 text-secondary align-middle align-middle">
+                                <div class="d-flex justify-content-evenly">
+                                    <div class="pr-2">
+                                        <a href="{{ route('cms.apb.edit', $item->id) }}" class="btn btn-info btn-sm me-3">View</a>
+                                    </div>
+                                    <div class="pr-2">
+                                        <a href="{{ $item->tahun !== null ? route('cms.apb.edit', $item->id) : route('cms.apb.editPengeluaran', $item->id) }}" class="btn btn-primary btn-sm me-3">Edit</a>
+                                    </div>
+
+                                    <form action="{{ route('cms.apb.destroy', $item->id) }}" method="post">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-danger btn-sm">Hapus</button>
+                                    </form>
+                                </div>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+            <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+            <script>
+                $(document).ready(function() {
+                    $('#searchInput').on('keyup', function() {
+                        var value = $(this).val().toLowerCase();
+                        $('#informasiTable tbody tr').filter(function() {
+                            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+                        });
+                    });
+                });
+            </script>
+        </div>
     </div>
+</div>
 @endsection
 
 @push('addon-style')
-    <link href="{{ url('') }}/cms/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+<link href="{{ url('') }}/cms/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
 @endpush
 @push('addon-script')
-    <!-- Page level plugins -->
-    <script src="{{ url('') }}/cms/vendor/datatables/jquery.dataTables.min.js"></script>
-    <script src="{{ url('') }}/cms/vendor/datatables/dataTables.bootstrap4.min.js"></script>
+<!-- Page level plugins -->
+<script src="{{ url('') }}/cms/vendor/datatables/jquery.dataTables.min.js"></script>
+<script src="{{ url('') }}/cms/vendor/datatables/dataTables.bootstrap4.min.js"></script>
 
-    <!-- Page level custom scripts -->
-    <script src="{{ url('') }}/cms/js/demo/datatables-demo.js"></script>
+<!-- Page level custom scripts -->
+<script src="{{ url('') }}/cms/js/demo/datatables-demo.js"></script>
 @endpush
